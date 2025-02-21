e28ef8c5568377cd7bcb7f7abe03dfbcfca188f4
Mohammed Essalih <esslihm1@gmail.com>
2025-02-21 09:03:10 +0100

    come corrections and improvements

--- Functions.R
+++ Functions.R
@@ -37,7 +37,8 @@ Gttt("U", Q_unif, PDF_unif, x = seq(0.01, 0.99, length.out = 100))
 # Generalized TTT function
 # ---------------------------------------------------------
 
-GTTT <- function(G, CDF, x) {
+GTTT <- function(G, CDF, Q, p) {
+  x=Q(p)
   if (G == "U") {
     return(x)  # Uniform case: TTT(x) = x
   }
@@ -56,27 +57,43 @@ GTTT <- function(G, CDF, x) {
 # ---------------------------------------------------------
 
 # Define CDF functions
-CDF_loglogistic <- function(x) pllogis(x, 1, 1)
-CDF_exponential <- function(x) pexp(x, 2)
+CDF <- function(x) pllogis(x,1,1)
+
+
+Q <- function(x) qllogis(x,1,1)
+
+
 
 # Generate x values
-x_vals <- seq(0.01, 0.99, length.out = 100)
+p <- seq(0.01, 0.99, length.out = 100)
 
-# Compute quantiles
-y_vals_loglogistic <- qllogis(x_vals, 1, 1)
-y_vals_exponential <- qexp(x_vals, 2)
+
+fun= function(x) {
+  GTTT(G = "LL", CDF = CDF,Q=Q, p = x)
+}
+
+
+plot(p,fun(p),type="l")
+
+DRV <- splinefun(p, grad(fun,p), method = "natural")
+
+plot(p,DRV(p),type="l",col="black")
+lines(p,p)
+
+lines(GTtt$x,DRV1_LCM(GTtt$x),type="l",col="black")
+lines(GTtt$x,DRV2_LCM(GTtt$x),type="l",col="red")
 
 # Function to plot GTTT results
 plot_GTTT <- function(G, CDF, y_vals, title) {
-  result <- GTTT(G = G, CDF = CDF, x = y_vals)
+  result <- GTTT(G = G, CDF = CDF,Q=Q, x = x_vals)
   plot(x_vals, result, type = "l", col = "blue", lwd = 2,
        main = title, xlab = "x", ylab = "GTTT(x)")
 }
 
 # Plot for different distributions
-plot_GTTT("U", CDF_loglogistic, y_vals_loglogistic, "GTTT for Uniform Case")
-plot_GTTT("LL", CDF_loglogistic, y_vals_loglogistic, "GTTT for Log-Logistic Case")
-plot_GTTT("EXP", CDF_exponential, y_vals_exponential, "GTTT for Exponential Case")
+plot_GTTT("U", CDF_loglogistic,Q_loglogistic, x_vals_loglogistic, "GTTT for Uniform Case")
+plot_GTTT("LL", CDF_loglogistic,Q_loglogistic, x_vals_loglogistic, "GTTT for Log-Logistic Case")
+plot_GTTT("EXP", CDF_exponential,Q_exponential, x_vals_exponential, "GTTT for Exponential Case")
 
 # ---------------------------------------------------------
 # Generalized Hazard Function (GHF) and Generalized Hazard Rate (GHR)
@@ -117,13 +134,15 @@ GHR <- function(G, CDF, PDF, x) {
 }
 
 # Define CDF and PDF functions for Log-Logistic(1,1)
-CDF_loglogistic <- function(x) pllogis(x, 1, 1)
-PDF_loglogistic <- function(x) dllogis(x, 1, 1)
+CDF<- function(x) pllogis(x, 0.5, 1)
+PDF<- function(x) dllogis(x, 0.5, 1)
 
 # Compute and plot the difference GHF("LL", CDF, x) - x
 x_vals <- seq(0.01, 5, length.out = 100)
-ghf_vals <- sapply(x_vals, function(x) GHF("LL", CDF_loglogistic, x))
+ghf_vals <- sapply(x_vals, function(x) GHR("LL", CDF,PDF, x))
 
 plot(x_vals, ghf_vals, type = "l", col = "red", lwd = 2,
      main = "GHF - x for Log-Logistic Case", xlab = "x", ylab = "GHF(x) - x")
+
+
 grid()
